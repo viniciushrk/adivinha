a40ae7cd78034fef685462addda5f143ef690aa8
viniciushrk <viniciusrosa444@gmail.com>
2019-02-20 09:38:29 -0300

    finalizado

--- app/src/main/java/com/adivina/viniciushrk/adivinha/MainActivity.java
+++ app/src/main/java/com/adivina/viniciushrk/adivinha/MainActivity.java
@@ -2,12 +2,40 @@ package com.adivina.viniciushrk.adivinha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = (Button) findViewById(R.id.botaojogarid);
+        textoResultado = (TextView) findViewById(R.id.resultadoid);
+
+        //textoResultado.setText("Texto alterado");
+        botaoJogar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Random randomico = new Random();
+
+                int numeroAleatorio = randomico.nextInt(11);
+
+                textoResultado.setText("número escolhido: " + numeroAleatorio);
+            }
+        });
+
+
     }
 }
